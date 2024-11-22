e8e5c23c03ccab29f05604896378efb5d50c7925
SAIRANTRI <pathaksairantri@gmail.com>
2024-11-22 08:13:41 +0530

    added upload facilities and update profilePic endpoint

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import bcyrpt from "bcryptjs";
 import { generateToken } from "../lib/utils.js"; 
 import e from "express";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { email, fullName, password } = req.body;
@@ -73,5 +74,28 @@ export const logout = (req, res) => {
 };
 
 export const updateProfile = async (req, res) => {
-    
+    try {
+      const {profilePic} = req.body;
+      const userId = req.user._id;
+
+      if(!profilePic) return res.status(400).json({ message: "Profile picture is required" });
+
+      const uploadResponse = await cloudinary.uploader.upload(profilePic);
+      const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      console.log("Error in updateProfile controller", error.message);
+      res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
+
+
