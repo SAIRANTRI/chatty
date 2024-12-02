11f0ba916c0d5d6b6ef7aa7411a59f5d0305aff6
SAIRANTRI <pathaksairantri@gmail.com>
2024-12-02 12:55:46 +0530

    added send/get message functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,10 @@ import { connectDB } from './lib/db.js'
 import cookieParser from "cookie-parser"
 import cors from "cors";
 
+import path from "path";
+import { app, server } from './lib/socket.js'
+
 dotenv.config();
-const app = express();
 
 
 app.use(express.json());
@@ -22,7 +24,7 @@ const PORT = process.env.PORT || 5001;
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server started on port " + PORT);
     connectDB();
 })
