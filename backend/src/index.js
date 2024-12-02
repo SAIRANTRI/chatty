ebc2cda090077286ef98c52a8b0736a2ebcad852
SAIRANTRI <pathaksairantri@gmail.com>
2024-12-02 07:58:35 +0530

    added theme settings and home page - sidebar

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(cors({
 const PORT = process.env.PORT || 5001;
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("Server started on port " + PORT);
