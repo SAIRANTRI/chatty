59fd8603b79238f3d49a22cc470e401debed8b62
SAIRANTRI <pathaksairantri@gmail.com>
2024-12-03 09:46:43 +0530

    completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,6 +24,14 @@ const PORT = process.env.PORT || 5001;
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
+
 server.listen(PORT, () => {
     console.log("Server started on port " + PORT);
     connectDB();
