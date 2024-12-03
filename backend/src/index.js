eeb96594bbc948db7d25cf948e04323a08240a86
SAIRANTRI <pathaksairantri@gmail.com>
2024-12-03 10:06:53 +0530

    final touches

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,20 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import authRoutes from './routes/auth.routes.js'
-import messageRoutes from './routes/message.routes.js'
-import { connectDB } from './lib/db.js'
-import cookieParser from "cookie-parser"
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import { connectDB } from './lib/db.js';
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path";
-import { app, server } from './lib/socket.js'
+import { fileURLToPath } from "url";
+import { app, server } from './lib/socket.js';
 
 dotenv.config();
 
+// Get __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -30,9 +34,9 @@ if (process.env.NODE_ENV === "production") {
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
-  }
+}
 
 server.listen(PORT, () => {
     console.log("Server started on port " + PORT);
     connectDB();
-})
+});
